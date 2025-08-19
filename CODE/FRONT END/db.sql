3225f246d34278b5e71bd49e22e4183f0e470249
Shaik Suleman <shaiksuleman2277@gmail.com>
2025-08-19 16:58:51 +0530

    Initial commit: Multiperspective Fraud Detection project

---
+++ CODE/FRONT END/db.sql
@@ -0,0 +1,16 @@
+-- first execute this three line queries only
+
+drop database db;
+create database db;
+use db;
+
+-- after running project execute the below queries
+
+insert into products (name, price, path) values ("example", 100, "static/images/p1.png");
+insert into products (name, price, path) values ("example", 100, "static/images/p2.png");
+insert into products (name, price, path) values ("example", 100, "static/images/p3.png");
+insert into products (name, price, path) values ("example", 100, "static/images/p4.png");
+insert into products (name, price, path) values ("example", 100, "static/images/p5.png");
+insert into products (name, price, path) values ("example", 100, "static/images/p6.png");
+insert into products (name, price, path) values ("example", 100, "static/images/p7.png");
+insert into products (name, price, path) values ("example", 100, "static/images/p8.png");
